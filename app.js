4fa5292f7f325aa0d53ebc3c5bbf0fd367bb3de9
Diana Clabel <dianaclabel08@gmail.com>
2022-05-12 02:53:17 -0500

    modified Javascript

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 let inputEl = document.getElementById("input-encriptador");
 let ouput = document.getElementById("input--copy");
 
+/*function messageEncriptado() {
+  let encriptado = encriptar();
+  ouput.value = encriptado;
+  inputEl.value = "";
+}
+function messageDesencriptado() {
+  let desencriptado = desencriptar();
+  ouput.value = desencriptado;
+  inputEl.value = "";
+}*/
+
 function encriptar() {
   let entrada = inputEl.value;
   let wordMinuscula = entrada.toLowerCase();
-
   for (let i = 0; i < 5; i++) {
     let vocals = ["a", "e", "i", "o", "u"];
     let words = ["ai", "enter", "imes", "ober", "ufac"];
@@ -12,6 +22,28 @@ function encriptar() {
     if (wordMinuscula.includes(vocals[i])) {
       wordMinuscula = wordMinuscula.replaceAll(vocals[i], words[i]);
     }
-    console.log(wordMinuscula);
   }
+  ouput.value = wordMinuscula;
+  inputEl.value = "";
+}
+
+function desencriptar() {
+  let entrada = inputEl.value;
+  let wordMinuscula = entrada.toLowerCase();
+
+  for (let i = 0; i < 5; i++) {
+    let vocals = ["a", "e", "i", "o", "u"];
+    let words = ["ai", "enter", "imes", "ober", "ufac"];
+
+    if (wordMinuscula.includes(words[i])) {
+      wordMinuscula = wordMinuscula.replaceAll(words[i], vocals[i]);
+    }
+  }
+  ouput.value = wordMinuscula;
+  inputEl.value = "";
+}
+
+function copy() {
+  ouput.select();
+  document.execCommand("copy");
 }
